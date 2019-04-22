ac4752728fa5fdbd2d7179491f854d6e46515fac
akmaldevtron <m.akmal.khan@devtrongenesis.com>
2019-04-22 19:49:14 +0500

    Download And Install Commerce Module Using Composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -117,6 +117,7 @@ class ComposerStaticInitDrupal8
         array (
             'Composer\\Semver\\' => 16,
             'Composer\\Installers\\' => 20,
+            'CommerceGuys\\Intl\\' => 18,
             'CommerceGuys\\Addressing\\' => 24,
         ),
         'B' => 
@@ -414,6 +415,10 @@ class ComposerStaticInitDrupal8
         'Composer\\Installers\\' => 
         array (
             0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+        'CommerceGuys\\Intl\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/commerceguys/intl/src',
         ),
         'CommerceGuys\\Addressing\\' => 
         array (
