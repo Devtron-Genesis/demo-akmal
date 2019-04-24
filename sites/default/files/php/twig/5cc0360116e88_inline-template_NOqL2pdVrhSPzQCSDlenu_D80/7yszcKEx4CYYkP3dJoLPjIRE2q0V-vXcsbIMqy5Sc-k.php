572ccbfe2ba9c8abaa001baff109f40778018729
akmaldevtron <m.akmal.khan@devtrongenesis.com>
2019-04-24 19:41:35 +0500

    Commerce Module Use

---
+++ sites/default/files/php/twig/5cc0360116e88_inline-template_NOqL2pdVrhSPzQCSDlenu_D80/7yszcKEx4CYYkP3dJoLPjIRE2q0V-vXcsbIMqy5Sc-k.php
@@ -0,0 +1,87 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* {# inline_template_start #}<p><em>{{ purchased_entity_label }}</em> added to the cart.</p> */
+class __TwigTemplate_d2decc7e6c0253f6f88d007a302ae3ccd0c5d32c08a3a80c9416248cf91febf1 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<p><em>";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["purchased_entity_label"] ?? null)), "html", null, true);
+        echo "</em> added to the cart.</p>";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<p><em>{{ purchased_entity_label }}</em> added to the cart.</p>";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "{# inline_template_start #}<p><em>{{ purchased_entity_label }}</em> added to the cart.</p>", "");
+    }
+}
